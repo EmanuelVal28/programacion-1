6db98e2ea00d5281335f3cb39f63e52f39be4499
EmanuelVal28 <emanuelvalencia@estudiante.uniajc.edu.co>
2025-09-01 20:47:07 +0000

    Realizacion de los puntos en clase

---
+++ clase 5/cuatropuntoa.java
@@ -0,0 +1,8 @@
+public class cuatropuntoa {public static void main(String[] args) {
+    int angulo=90;
+    if (angulo==90) { System.out.println("El angulo es recto" );
+        
+    } else {System.out.println("El angulo no es recto");
+    }
+}
+}
