91dde3c595dd84bcdafe194d16c7ad44e23c1169
EmanuelVal28 <emanuelvalencia@estudiante.uniajc.edu.co>
2025-09-29 17:42:04 +0000

    doce

---
+++ tabla.java
@@ -0,0 +1,7 @@
+public class tabla { public static void main(String[] args) {
+    int x=5;
+    for (int i = 1; i <= 15; i++) {
+        System.out.println(x + " * " + i + " = " + (x * i));
+    }
+}
+}
